e0f015c883d58ef61981271ea3ce5185cf559617
SebRamsland <mail@sebastianramsland.com>
2026-06-12 12:31:18 +0200

    feat: Support for suspending functions

--- http4k-openapi/build.gradle.kts
+++ http4k-openapi/build.gradle.kts
@@ -5,6 +5,7 @@ plugins {
 dependencies {
     api(libs.http4k.core)
     api(libs.http4k.api.openapi)
+    api(libs.kotlinx.coroutines.core)
     api(libs.http4k.format.kotlinx.serialization)
     implementation(libs.kotlinx.serialization.json)
     api(project(":http4k-errors"))
