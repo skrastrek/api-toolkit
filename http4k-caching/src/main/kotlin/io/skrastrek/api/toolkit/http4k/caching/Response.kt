e1300c118b4d16b3f6b285c3ff85dc2452200d4a
Sebastian Ramsland <SebRamsland@users.noreply.github.com>
2026-06-30 15:09:30 +0200

    feat: Add proxy-revalidate extension on Response (http4k) (#9)

--- http4k-caching/src/main/kotlin/io/skrastrek/api/toolkit/http4k/caching/Response.kt
+++ http4k-caching/src/main/kotlin/io/skrastrek/api/toolkit/http4k/caching/Response.kt
@@ -16,3 +16,13 @@ fun <T : Versionable> List<T>.notModifiedResponse(seed: String = "") =
         .eTag(this, seed)
 
 fun Response.contentLocation(uri: Uri) = with(Header.CONTENT_LOCATION of uri)
+
+fun Response.proxyRevalidate() = replaceHeader("Cache-Control", "proxy-revalidate".ensureOnlyOnceIn(header("Cache-Control")))
+
+private fun String.ensureOnlyOnceIn(currentValue: String?): String =
+    currentValue
+        ?.split(",")
+        ?.map(String::trim)
+        ?.toSet()
+        ?.plus(this)
+        ?.joinToString(", ") ?: this
